23862ec8254b7197bcee5f72012ddd7f967e7877
Patrik <patrikvonmalottki@proton.me>
2025-04-30 13:17:27 +0200

    Trying to implement custom package;

--- java/unterricht/reflect/src/main/ReflectMain.java
+++ java/unterricht/reflect/src/main/ReflectMain.java
@@ -5,7 +5,13 @@ import java.lang.reflect.*;
 public class ReflectMain {
 
 	public static void main(String[] args) {
-		String className = ReflectMain.class.getName();
-		System.out.println(className);
+		try {
+			Class xyz = Class.forName(("reflect.src.main.UnbekannteKlasse"));
+
+			String className = xyz.getName();
+			System.out.println(className);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 }
