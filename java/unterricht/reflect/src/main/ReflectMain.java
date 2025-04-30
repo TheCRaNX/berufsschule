fca45f7fd3280d9f44e3759695ca0a50fc8feb82
Patrik <patrikvonmalottki@proton.me>
2025-04-30 13:09:02 +0200

    Add reflect exercise;

---
+++ java/unterricht/reflect/src/main/ReflectMain.java
@@ -0,0 +1,11 @@
+package reflect.src.main;
+
+import java.lang.reflect.*;
+
+public class ReflectMain {
+
+	public static void main(String[] args) {
+		String className = ReflectMain.class.getName();
+		System.out.println(className);
+	}
+}
