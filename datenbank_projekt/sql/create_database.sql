cdfb1bb5b4b03f677ce30cff122932b234bdb34b
Patrik von Malottki <patrikvonmalottki@proton.me>
2025-06-24 02:46:07 +0200

    Rename to quartet;

--- datenbank_projekt/sql/create_database.sql
+++ datenbank_projekt/sql/create_database.sql
@@ -1,7 +1,7 @@
-create database motorraeder
+create database quartett
 character set utf8mb4
 collate utf8mb4_unicode_ci;
 
-use motorraeder;
+use quartett;
 
 set global log_bin_trust_function_creators = 1;
